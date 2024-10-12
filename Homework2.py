24027016621efb36ed0515b06dad8808bb5832c9
Darknet345 <141551506+Darknet345@users.noreply.github.com>
2024-10-12 15:35:45 +0400

    Create Homework2.py

---
+++ Homework2.py
@@ -0,0 +1,11 @@
+name = ("Name: Egor")
+print(name)
+name = ("Age: 18")
+age = name
+print(age)
+name = ("New_Age: 19")
+new_age = name
+print(new_age)
+name = ("Is_Student: True")
+is_student = name
+print(is_student)
